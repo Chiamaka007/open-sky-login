6d95f4cb38a8837f282bf972aed98a4018fb7243
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-04-14 17:29:10 +0100

    update app

--- src/components/InnerDashboard.jsx
+++ src/components/InnerDashboard.jsx
@@ -1,6 +1,16 @@
 import React, { Component, Fragment } from "react";
+import axios from "axios";
 
+const api = axios.create({
+    baseURL: "https://opensky-network.org/api/flights/arrival?airport=EDDF&begin=1517227200&end=1517230800"
+})
 class InnerDashboard extends Component {
+    constructor() {
+        super();
+        api.get('/').then(res => {
+            console.log(res.data)
+        })
+    }
   render() {
     return (
       <Fragment>
